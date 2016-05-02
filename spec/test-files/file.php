56afa459dc072933d507b83d8f3119812d132a30
fracalo <premispezia@gmail.com>
2016-05-02 18:55:13 +0200

    outsourced search functionality - added scss implicit support

--- spec/test-files/file.php
+++ spec/test-files/file.php
@@ -2,7 +2,11 @@
     $Name ='franky';
     print "<html>\n";
     print "<body>\n";
-    print "<p>Welcome, $Name</P>\n";
-    print "</body>\n";
+    print "<p>Welcome, $Name</P>\n";    print "</body>\n";
     print "</html>\n";
+
+    function add/*: float*/($a, $b){
+      return $a + $b
+    }
+    // $z = add(3,3)
 ?>
